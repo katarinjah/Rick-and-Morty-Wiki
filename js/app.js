cac015cdfbe4f67f9a775596998aa1d5e4056702
katarinjah <katarina.d.262@gmail.com>
2023-02-24 19:44:19 +0100

    Added Like and Back button functionalities

--- js/app.js
+++ js/app.js
@@ -6,19 +6,32 @@ window.onload = function() {
 };
 
 const onSingleCharacterClick = e => {
-    const targetEl = e.target.parentNode.parentNode;
-    if (!targetEl.classList.contains("character-item")) {
-      return;
-    };
-    const id = targetEl.getAttribute("id");
-    getSingleCharacter(id)
-      .then(character => {
-        renderSingleCharacter(character);
-      });
+  const targetEl = e.target.closest('.card-top');
+  const id = targetEl.parentNode.parentNode.getAttribute('id');
+  getSingleCharacter(id)
+    .then(character => {
+      renderSingleCharacter(character);
+    });
+};
+
+const onLikeButtonClick = e => {
+  const button = e.target;
+  if (!button.classList.contains("like-button")) {
+    return;
+  }
+  button.classList.toggle("liked");
+};
+
+const onBackButtonClick = e => {
+  const targetEl = e.target.parentNode.parentNode.parentNode;
+  if (!targetEl.classList.contains("character-box")) {
+    return;
   };
-  
+  getCharacters().then(character => {renderCharacters(character)});
+};
 
 mainContainer.addEventListener("click", onSingleCharacterClick);
-
+mainContainer.addEventListener("click", onLikeButtonClick);
+mainContainer.addEventListener("click", onBackButtonClick)
 
 
--- js/ui.js
+++ js/ui.js
@@ -6,9 +6,13 @@ export const renderCharacters = characters => {
     html += `
       <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 character-item d-flex justify-content-center" id="${character.id}">
         <div class="card" style="width: 50%">
-          <img src="${character.image}" class="card-img-top" alt="character image">
-            <p class="card-text">${character.name}</p>
-            <button style="font-size:15px"><i style="font-size:15px" class="fa">&#xf087;</i> Like</button>
+          <div class="card-top">
+           <img src="${character.image}" class="card-img-top" alt="character image">
+           <p class="card-text">${character.name}</p>
+          </div>
+          <div class="card-bottom">
+            <button class="like-button" style="font-size:15px"><i style="font-size:15px" class="fa">&#xf087;</i> Like</button>
+          </div>
         </div>
       </div>
       `;
@@ -28,36 +32,39 @@ export const renderSingleCharacter = character => {
     <div class="row justify-content-center">
       <img class="char-img" src="${character.image}">
     </div>
-    <div class="details">
-      <div class="row">
+    <div class="details overflow-hidden">
+      <div class="row justify-content-center">
         <div class="col-sm-3">
           <h5>Status: </h5>
         </div>
         <div class="col-sm-9">${character.status}</div>
       </div>
-      <div class="row">
+      <div class="row justify-content-center">
         <div class="col-sm-3">
           <h5>Species: </h5> 
         </div>
         <div class="col-sm-9">${character.species}</div>
       </div>
-      <div class="row">
+      <div class="row justify-content-center">
         <div class="col-sm-3">
           <h5>Gender: </h5> 
         </div>
         <div class="col-sm-9">${character.gender}</div>
       </div>
-      <div class="row">
+      <div class="row justify-content-center">
         <div class="col-sm-3">
           <h5>Origin: </h5> 
         </div>
         <div class="col-sm-9">${character.origin.name}</div>
       </div>
-      <div class="row">
+      <div class="row justify-content-center">
         <div class="col-sm-3">
           <h5>Location: </h5>
         </div>
         <div class="col-sm-9">${character.location.name}</div>
+      </div>
+      <div class="row justify-content-center">
+        <button id="back-button">Back</button>
       </div>
     </div>
   </div>
